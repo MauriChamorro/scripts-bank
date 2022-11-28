422ede087868eead42ef295cb05b1ed601a0e0d3
Mauri Chamorro <mmchamoo@gmail.com>
2022-11-28 11:39:26 -0300

    first commit

---
+++ Time/TimeSpan.cs
@@ -0,0 +1,12 @@
+namespace ScriptsBank.Time;
+
+public class TimeSpan
+{
+    public TimeSpan()
+    {
+        // var txtTime = _currentTime.Hours > 0 ? $"{_currentTime.Hours}h {_currentTime.Minutes}'" 
+        //     : $"{_currentTime.Minutes}' {_currentTime.Seconds}''";
+        // var txtTime = _currentTime.Hours > 0 ? _currentTime.ToString(@"hh\:mm")
+        //     : _currentTime.ToString(@"mm\:ss");   
+    }
+}
